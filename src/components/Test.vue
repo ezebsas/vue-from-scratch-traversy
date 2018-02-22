0cd246f84be0db52007f52ee09650fd9c7f38ecb
eze <mollinedoezequiel@gmail.com>
2018-02-22 15:38:37 -0300

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="test">
+    <h1>{{title}}</h1>
+    <p>{{user.firstName}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data() {
+      return {
+        title: 'Hello world',
+        user: {
+          firstName: 'John',
+          lastName: 'Doe'
+        }
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+
+</style>
